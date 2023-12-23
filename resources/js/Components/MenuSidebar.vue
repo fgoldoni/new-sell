a117a9b000b14608c9cf322542ce2dfa208cb0a4
Goldoni <g.fouotsa@demv.de>
2023-12-23 10:38:57 +0100

    initial commit

--- resources/js/Components/MenuSidebar.vue
+++ resources/js/Components/MenuSidebar.vue
@@ -72,9 +72,45 @@
                                         class="flex grow flex-col gap-y-5 overflow-y-auto bg-white dark:bg-slate-800 px-6 pb-4"
                                     >
                                         <nav class="flex flex-1 flex-col mt-4">
+                                            <div
+                                                class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+                                            >
+                                                <div class="flex-shrink-0">
+                                                    <img
+                                                        class="h-10 w-10 rounded-full"
+                                                        :src="imageUrl"
+                                                        alt=""
+                                                    />
+                                                </div>
+                                                <div class="min-w-0 flex-1">
+                                                    <a
+                                                        href="#"
+                                                        class="focus:outline-none"
+                                                    >
+                                                        <span
+                                                            class="absolute inset-0"
+                                                            aria-hidden="true"
+                                                        />
+                                                        <p
+                                                            class="text-sm font-medium text-gray-900"
+                                                        >
+                                                            {{
+                                                                __(
+                                                                    "Goldoni Fouotsa",
+                                                                )
+                                                            }}
+                                                        </p>
+                                                        <p
+                                                            class="truncate text-sm text-gray-500"
+                                                        >
+                                                            fotsa.goldoni@yahoo.fr
+                                                        </p>
+                                                    </a>
+                                                </div>
+                                            </div>
                                             <ul
                                                 role="list"
-                                                class="flex flex-1 flex-col gap-y-7"
+                                                class="flex flex-1 flex-col gap-y-7 mt-4"
                                             >
                                                 <li>
                                                     <ul
@@ -252,12 +288,23 @@ import {
     ArrowRightOnRectangleIcon,
     Cog6ToothIcon,
     ShoppingCartIcon,
-    UserCircleIcon,
     XMarkIcon,
 } from "@heroicons/vue/24/outline";
 
+const imageUrl =
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
+const people = [
+    {
+        name: "Leslie Alexander",
+        email: "leslie.alexander@example.com",
+        role: "Co-Founder / CEO",
+        imageUrl:
+            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    // More people...
+];
+
 const navigation = [
-    { name: "Goldoni Fouotsa", href: "#", icon: UserCircleIcon, current: true },
     {
         name: "Meine Bestellungen",
         href: "#",
